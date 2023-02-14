7c388e4e933c074b08c786ce656ff10d084371d3
Maximilian Paju <maximilian.paju@gmail.com>
2023-02-14 14:41:10 +0100

    fix: deployment of resources to azure, setting up CI/CD

--- remix.init/index.js
+++ remix.init/index.js
@@ -4,7 +4,6 @@ const fs = require("fs/promises");
 const path = require("path");
 const inquirer = require("inquirer");
 const { EOL } = require("os");
-
 const sort = require("sort-package-json");
 
 const setupGitRepository = require("./setup-git-repository");
@@ -27,11 +26,19 @@ function getRandomPassword(
     .join("");
 }
 
-async function main({ rootDirectory }) {
+async function main({ rootDirectory, ...rest }) {
+  console.log("Starting Remix template...", rootDirectory, rest);
+
   const readmePath = path.join(rootDirectory, "README.md");
   const exampleEnvPath = path.join(rootDirectory, ".env.example");
   const envPath = path.join(rootDirectory, ".env");
   const packageJsonPath = path.join(rootDirectory, "package.json");
+  const deployYmlPath = path.join(
+    rootDirectory,
+    ".github",
+    "workflows",
+    "deploy.yml"
+  );
 
   const dirName = path.basename(rootDirectory);
   const appName = dirName.replace(/-/g, "").slice(0, 12);
@@ -47,20 +54,23 @@ async function main({ rootDirectory }) {
   const sqlServerPassword = getRandomPassword();
   const sqlServerUsername = appName;
 
-  const configuration = [
-    { name: "azure_subscription_id", value: azureSubscriptions[0].id },
-    { name: "web_site_name", value: appName },
-    { name: "sql_server_name", value: appName },
-    { name: "web_serverfarms_name", value: appName },
-    { name: "container_registry_name", value: appName },
-    { name: "container_registry_image_name_and_label", value: appName },
-    { name: "container_registry_username", value: "Username.1" },
-    { name: "sql_server_admin_username", value: sqlServerUsername },
-    { name: "sql_server_admin_password", value: sqlServerPassword },
+  const subscriptionId = azureSubscriptions[0].id;
+  const tenantId = azureSubscriptions[0].tenantId;
+
+  const parametersSearchReplace = [
+    { search: "azure_subscription_id", replace: subscriptionId },
+    { search: "web_site_name", replace: appName },
+    { search: "sql_server_name", replace: appName },
+    { search: "web_serverfarms_name", replace: appName },
+    { search: "container_registry_name", replace: appName },
+    { search: "container_registry_image_name_and_label", replace: appName },
+    { search: "container_registry_username", replace: "Username.1" },
+    { search: "sql_server_admin_username", replace: sqlServerUsername },
+    { search: "sql_server_admin_password", replace: sqlServerPassword },
   ];
 
-  configuration.forEach((parameter) => {
-    parametersJSONFile.parameters[parameter.name].value = parameter.value;
+  parametersSearchReplace.forEach((parameter) => {
+    parametersJSONFile.parameters[parameter.search].value = parameter.replace;
   });
 
   await fs.writeFile(
@@ -74,16 +84,20 @@ async function main({ rootDirectory }) {
   );
   console.log("Created new resource group", resourceGroup);
 
+  const resourceGroupName = resourceGroup.name;
+
   console.log(
     "Deploying stack to Azure with parameters...",
     parametersJSONFile
   );
   const deployment = JSON.parse(
     execSync(
-      `az deployment group create --template-file ${__dirname}/azure/template.json --parameters @${__dirname}/azure/replaced_parameters.json --resource-group ${resourceGroup.name} --name ${appName}`
+      `az deployment group create -g ${resourceGroupName} --template-file ${__dirname}/azure/template.json --parameters @${__dirname}/azure/replaced_parameters.json --name ${appName}`
     )
   );
   console.log("Success!", JSON.stringify(deployment, null, 2));
+
+  var outputs = deployment.properties.outputs;
 
   console.log("Setting up container registry access...");
   const acrRegistryId = execSync(
@@ -195,13 +209,41 @@ async function main({ rootDirectory }) {
       2
     ) + "\n";
 
-  console.log(`Updating template files with what you've told us`);
+  const newDeployYml = deployYml;
+  const deployYmlSearchReplace = [
+    {
+      search: "${AZURE_WEBAPP_NAME}",
+      replace: appName,
+      search: "${AZURE_REGISTRY_URL}",
+      replace: outputs.containerRegistryPrincipal,
+      search: "${AZURE_SUBSCRIPTION_ID}",
+      replace: subscriptionId,
+      search: "${AZURE_TENTANT_ID}",
+      replace: tenantId,
+      search: "${IMAGE_NAME}",
+      replace: appName,
+    },
+  ];
+
+  deployYmlSearchReplace.forEach((replace) => {
+    newDeployYml.replace(
+      new RegExp(escapeRegExp(replace.name), "g"),
+      replace.content
+    );
+  });
 
   await Promise.all([
     fs.writeFile(readmePath, newReadme),
     fs.writeFile(envPath, newEnv),
     fs.writeFile(packageJsonPath, newPackageJson),
+    fs.writeFile(deployYmlPath, newDeployYml),
   ]);
+
+  await Promise.all([fs.rm(path.join(rootDirectory, "LICENSE.md"))]);
+
+  console.log(
+    `Now commit and push your code to your Github repository and check that the Github Action completes.`
+  );
 
   console.log(`Removing temporary files from disk.`);
 
